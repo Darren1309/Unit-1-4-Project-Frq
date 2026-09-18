6c510d2e9e4f98a2c214d5d2c47c62122171513b
Darren1309 <darren.chhlang@student.rjuhsd.us>
2026-09-18 12:26:46 -0700

    cafe

---
+++ Cafe.java
@@ -0,0 +1,34 @@
+import java.util.Scanner();
+
+public class Cafe{
+    public static void main(String[] args){
+        Scanner i = new Scanner(System.in);
+        System.out.println("Welcome to your Cafe!");
+        System.out.println("Please enter the name for your Cafe: ");
+        String cafeName = i.nextLine();
+        System.out.println("Welcome to the " + cafeName + " Cafe!");
+        int coffeeBeans = 0;
+        int milk = 0;
+        int muffins = 0;
+
+        int turkey = 0;
+        int tomatoes = 0;
+        int lettuce = 0;
+        int bread = 0;
+
+        
+        int pancakes = 0;
+        int waffles = 0;
+        
+        int tortillas = 0;
+        int sausage = 0;
+        int potatoes = 0;
+        int cheese = 0;
+        int eggs = 0;
+        
+        int bagels = 0;
+
+
+
+    }
+}
